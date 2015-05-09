f25d200f3e66ad258431af198a1e227f0525fcc2
Paul Tagliamonte <tag@pault.ag>
2015-05-09 00:16:03 -0400

    add test start

---
+++ version/version_test.go
@@ -0,0 +1,63 @@
+/* Copyright (c) Paul R. Tagliamonte <paultag@debian.org>, 2015
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE. */
+
+package version_test
+
+import (
+	"testing"
+
+	"pault.ag/x/go-debian/version"
+)
+
+/*
+ *
+ */
+
+func isok(t *testing.T, err error) {
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func notok(t *testing.T, err error) {
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func assert(t *testing.T, expr bool) {
+	if !expr {
+		t.Fail()
+	}
+}
+
+/*
+ *
+ */
+
+func TestSingleParse(t *testing.T) {
+	ver, err := version.Parse("1:1.0-1")
+	isok(t, err)
+
+	assert(t, ver.Epoch == 1)
+	assert(t, ver.Version == "1.0")
+	assert(t, ver.Revision == "1")
+
+}
