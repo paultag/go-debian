e61025d0a87d8ec9f19686cf81469d77cbb33572
Paul Tagliamonte <tag@pault.ag>
2015-08-13 15:34:32 +0200

    Add in more bits.

--- control/index.go
+++ control/index.go
@@ -33,13 +33,14 @@ type BinaryIndex struct {
 	Package        string
 	Source         string
 	Version        version.Version
-	InstalledSize  string
+	InstalledSize  string `control:"Installed-Size"`
 	Maintainer     string
 	Architecture   dependency.Arch
+	MultiArch      string `control:"Multi-Arch"`
 	Description    string
 	Homepage       string
-	DescriptionMD5 string
-	Tags           []string
+	DescriptionMD5 string   `control:"Description-md5"`
+	Tags           []string `delim:", "`
 	Section        string
 	Priority       string
 	Filename       string
@@ -53,6 +54,22 @@ func (index *BinaryIndex) GetDepends() dependency.Dependency {
 	return index.getOptionalDependencyField("Depends")
 }
 
+func (index *BinaryIndex) GetSuggests() dependency.Dependency {
+	return index.getOptionalDependencyField("Suggests")
+}
+
+func (index *BinaryIndex) GetBreaks() dependency.Dependency {
+	return index.getOptionalDependencyField("Breaks")
+}
+
+func (index *BinaryIndex) GetReplaces() dependency.Dependency {
+	return index.getOptionalDependencyField("Replaces")
+}
+
+func (index *BinaryIndex) GetPreDepends() dependency.Dependency {
+	return index.getOptionalDependencyField("Pre-Depends")
+}
+
 type SourceIndex struct {
 	Paragraph
 
@@ -61,6 +78,7 @@ type SourceIndex struct {
 
 	Version    version.Version
 	Maintainer string
+	Uploaders  string `delim:","`
 
 	Architecture []dependency.Arch
 
@@ -69,17 +87,12 @@ type SourceIndex struct {
 	Files            []string `delim:"\n"`
 	VcsBrowser       string   `control:"Vcs-Browser"`
 	VcsGit           string   `control:"Vcs-Git"`
+	VcsSvn           string   `control:"Vcs-Svn"`
+	VcsBzr           string   `control:"Vcs-Bzr"`
 	Homepage         string
 	Directory        string
 	Priority         string
 	Section          string
-
-	/*
-		TODO:
-			Checksums-Sha1:
-			Checksums-Sha256:
-			Package-List:
-	*/
 }
 
 func (index *SourceIndex) GetBuildDepends() dependency.Dependency {
