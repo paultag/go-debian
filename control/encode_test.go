388d0c3b15499881e4cb483e7a495206714cccda
Paul Tagliamonte <tag@pault.ag>
2016-02-14 15:09:22 -0500

    Add Version

--- control/encode_test.go
+++ control/encode_test.go
@@ -7,11 +7,15 @@ import (
 
 	"pault.ag/go/debian/control"
 	// "pault.ag/go/debian/dependency"
-	// "pault.ag/go/debian/version"
+	"pault.ag/go/debian/version"
 )
 
 type TestMarshalStruct struct {
 	Foo string
+}
+
+type SomeVersionStruct struct {
+	Version version.Version
 }
 
 type TestParaMarshalStruct struct {
@@ -45,6 +49,20 @@ func TestBasicMarshal(t *testing.T) {
 
 	assert(t, testStruct.Foo == "Hello")
 	assert(t, writer.String() == `Foo: Hello
+
+`)
+}
+
+func TestExternalMarshal(t *testing.T) {
+	testStruct := SomeVersionStruct{}
+	isok(t, control.Unmarshal(&testStruct, strings.NewReader(`Version: 1.0-1
+`)))
+	writer := bytes.Buffer{}
+
+	err := control.Marshal(&writer, testStruct)
+	isok(t, err)
+
+	assert(t, writer.String() == `Version: 1.0-1
 
 `)
 }
