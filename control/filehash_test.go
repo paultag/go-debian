paultag/go-debian

commit dc799cd8b454fc8cfa3e741cd4bdd4e67b712e56

Add in Changes Unmarshal bits.

control/changes.go
@@ -41,6 +41,66 @@ type ChangesFileHash struct {
 	Filename  string
 }
 
+type FileListChangesFileHash struct {
+	ChangesFileHash
+
+	Component string
+	Priority  string
+}
+
+func (c *FileListChangesFileHash) UnmarshalControl(data string) error {
+	var err error
+	c.Algorithm = "md5"
+	vals := strings.Split(data, " ")
+	if len(data) < 2 {
+		return fmt.Errorf("Error: Unknown File List Hash line: '%s'", data)
+	}
+
+	c.Hash = vals[0]
+	c.Size, err = strconv.Atoi(vals[1])
+	if err != nil {
+		return err
+	}
+	c.Component = vals[2]
+	c.Priority = vals[3]
+
+	c.Filename = vals[4]
+	return nil
+}
+
+type SHAChangesFileHash struct {
+	ChangesFileHash
+}
+
+func (c *SHAChangesFileHash) unmarshalControl(algorithm, data string) error {
+	var err error
+	c.Algorithm = algorithm
+	vals := strings.Split(data, " ")
+	if len(data) < 2 {
+		return fmt.Errorf("Error: Unknown SHA Hash line: '%s'", data)
+	}
+
+	c.Hash = vals[0]
+	c.Size, err = strconv.Atoi(vals[1])
+	if err != nil {
+		return err
+	}
+	c.Filename = vals[2]
+	return nil
+}
+
+type SHA1ChangesFileHash struct{ SHAChangesFileHash }
+
+func (c *SHA1ChangesFileHash) UnmarshalControl(data string) error {
+	return c.unmarshalControl("SHA1", data)
+}
+
+type SHA256ChangesFileHash struct{ SHAChangesFileHash }
+
+func (c *SHA256ChangesFileHash) UnmarshalControl(data string) error {
+	return c.unmarshalControl("SHA256", data)
+}
+
 type Changes struct {
 	Paragraph
 
@@ -48,19 +108,19 @@ type Changes struct {
 
 	Format          string
 	Source          string
-	Binaries        []string
-	Architectures   []dependency.Arch
+	Binaries        []string          `control:"Binary" delim:" "`
+	Architectures   []dependency.Arch `control:"Architecture"`
 	Version         version.Version
 	Origin          string
 	Distribution    string
 	Urgency         string
 	Maintainer      string
-	ChangedBy       string
+	ChangedBy       string `control:"Changed-By"`
 	Closes          []string
 	Changes         string
-	ChecksumsSha1   []ChangesFileHash
-	ChecksumsSha256 []ChangesFileHash
-	Files           []ChangesFileHash
+	ChecksumsSha1   []SHA1ChangesFileHash     `control:"Checksums-Sha1" delim:"\n" strip:"\n\r\t "`
+	ChecksumsSha256 []SHA256ChangesFileHash   `control:"Checksums-Sha256" delim:"\n" strip:"\n\r\t "`
+	Files           []FileListChangesFileHash `control:"Files" delim:"\n" strip:"\n\r\t "`
 }
 
 func parseHashes(buf string, algorithm string) (ret []ChangesFileHash) {
@@ -114,49 +174,12 @@ func ParseChangesFile(path string) (ret *Changes, err error) {
 	return ret, nil
 }
 
-func ParseChanges(reader *bufio.Reader, path string) (ret *Changes, err error) {
-
-	/* a Changes is a Paragraph, with some stuff. So, let's first take
-	 * the bufio.Reader and produce a stock Paragraph. */
-	src, err := ParseParagraph(reader)
-	if err != nil {
+func ParseChanges(reader *bufio.Reader, path string) (*Changes, error) {
+	ret := &Changes{Filename: path}
+	if err := Unmarshal(ret, reader); err != nil {
 		return nil, err
 	}
-
-	arch, err := dependency.ParseArchitectures(src.Values["Architecture"])
-	if err != nil {
-		return nil, err
-	}
-
-	version, err := version.Parse(src.Values["Version"])
-	if err != nil {
-		return nil, err
-	}
-
-	ret = &Changes{
-		Paragraph: *src,
-		Filename:  path,
-
-		Format:     src.Values["Format"],
-		Source:     src.Values["Source"],
-		Origin:     src.Values["Origin"],
-		Urgency:    src.Values["Urgency"],
-		Maintainer: src.Values["Maintainer"],
-		ChangedBy:  src.Values["Changed-By"],
-		Changes:    src.Values["Changes"],
-
-		Architectures: arch,
-		Version:       version,
-		Distribution:  src.Values["Distribution"],
-
-		ChecksumsSha1:   parseHashes(src.Values["Checksums-Sha1"], "SHA1"),
-		ChecksumsSha256: parseHashes(src.Values["Checksums-Sha256"], "SHA256"),
-		Files:           parseHashes(src.Values["Files"], "MD5"),
-		Binaries:        strings.Split(src.Values["Binary"], " "),
-		Closes:          strings.Split(src.Values["Closes"], " "),
-	}
-
-	return
+	return ret, nil
 }
 
 func (changes *Changes) GetDSC() (*DSC, error) {

control/changes_test.go
@@ -129,6 +129,7 @@ Files:
 	// }}}
 	changes, err := control.ParseChanges(reader, "")
 	isok(t, err)
+
 	assert(t, len(changes.ChecksumsSha1) == 2)
 	assert(t, len(changes.ChecksumsSha256) == 2)
 	assert(t, len(changes.Files) == 2)

control/decode.go
@@ -43,6 +43,10 @@ func decodeCustomValues(incoming reflect.Value, incomingField reflect.StructFiel
 		strip = it
 	}
 
+	if strip != "" {
+		data = strings.Trim(data, strip)
+	}
+
 	for _, el := range strings.Split(data, delim) {
 		if strip != "" {
 			el = strings.Trim(el, strip)

commit 7437790ce3fce18d9875d96085fbe8516ce2ba5e

Don’t crash when delim is not specified
This code has originally been introduced in commit
dc799cd8b454fc8cfa3e741cd4bdd4e67b712e56, with the conditions updated in
82f1703f90851f06344b3f591b67997dc4cd3712.

fixes #83

control/changes.go
@@ -47,7 +47,7 @@ func (c *FileListChangesFileHash) UnmarshalControl(data string) error {
 	var err error
 	c.Algorithm = "md5"
 	vals := strings.Split(data, " ")
-	if len(data) < 5 {
+	if len(vals) < 5 {
 		return fmt.Errorf("Error: Unknown File List Hash line: '%s'", data)
 	}
 

control/filehash.go
@@ -125,7 +125,7 @@ func (c *FileHash) unmarshalControl(algorithm, data string) error {
 	var err error
 	c.Algorithm = algorithm
 	vals := strings.Fields(data)
-	if len(data) < 4 {
+	if len(vals) < 3 {
 		return fmt.Errorf("Error: Unknown Debian Hash line: '%s'", data)
 	}
 

control/filehash_test.go
@@ -1,0 +1,48 @@
+/* {{{ Copyright (c) Paul R. Tagliamonte <paultag@debian.org>, 2015
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE. }}} */
+
+package control_test
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"pault.ag/go/debian/control"
+)
+
+func TestCrashWithoutDelim(t *testing.T) {
+	type release struct {
+		MD5Sum []control.MD5FileHash
+		SHA1   []control.SHA1FileHash
+		SHA256 []control.SHA256FileHash
+	}
+	// Test Paragraph {{{
+	reader := bufio.NewReader(strings.NewReader(`MD5Sum:
+ b096313254606a2f984f6fc38e4769c8  1219120 contrib/Contents-amd64
+SHA256:
+ 0f6dc8c4865a433d321a3af0d4812030fdb14231cd1f93bd7570f1518a46faad  1219120 contrib/Contents-amd64
+`))
+	// }}}
+	var r release
+	if err := control.Unmarshal(&r, reader); err == nil {
+		t.Errorf("control.Unmarshal unexpectedly succeeded on struct without delim")
+	}
+}