94c0bc5f21a6adbff5c798a3bf22dc4ba8cc1a47
Charles Duffy <charles@dyfis.net>
2021-05-05 12:57:17 -0500

    Fix signature check to use modern format, drop test suite checking compliance with outdated format
    Since version 0.5, upstream debsig-verify has included `debian-binary` in the
    signed blob. However, its test suite never dropped files predating that change;
    in incorporating those files, this library was testing compliance with an
    outdated format.

--- deb/sigcheck.go
+++ deb/sigcheck.go
@@ -9,31 +9,41 @@ import (
 
 const (
 	SigTypeArchive = `archive`
-	SigTypeMaint = `maint`
-	SigTypeOrigin = `origin`
+	SigTypeMaint   = `maint`
+	SigTypeOrigin  = `origin`
 )
 
 func (deb *Deb) CheckDebsig(validKeys openpgp.EntityList, sigType string) (signer *openpgp.Entity, err error) {
-	sig, ok := deb.ArContent[`_gpg` + sigType]
+	sig, ok := deb.ArContent[`_gpg`+sigType]
 	if !ok {
 		return nil, fmt.Errorf("no signature of type %v present", sigType)
+	}
+
+	binaryFlag, ok := deb.ArContent[`debian-binary`]
+	if !ok {
+		return nil, fmt.Errorf("archive does not contain a debian-binary flag")
 	}
 
 	var control, data *ArEntry
 	for _, member := range deb.ArContent {
 		if strings.HasPrefix(member.Name, "control.") {
 			control = member
-			if data != nil { break }
+			if data != nil {
+				break
+			}
 		} else if strings.HasPrefix(member.Name, "data.") {
 			data = member
-			if control != nil { break }
+			if control != nil {
+				break
+			}
 		}
 	}
 	if control == nil || data == nil {
 		return nil, fmt.Errorf("unable to find signed data")
 	}
+	binaryFlag.Data.Seek(0, 0)
 	control.Data.Seek(0, 0)
 	data.Data.Seek(0, 0)
-	signedData := io.MultiReader(control.Data, data.Data)
+	signedData := io.MultiReader(binaryFlag.Data, control.Data, data.Data)
 	return openpgp.CheckDetachedSignature(validKeys, signedData, sig.Data)
 }
