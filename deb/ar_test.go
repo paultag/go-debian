7c070bf89d05b77b71f656e12eb6ef911b2b41fe
Richard Gomez <rmgomez368@gmail.com>
2024-06-23 15:51:46 -0400

    wip: add tests

--- deb/ar_test.go
+++ deb/ar_test.go
@@ -1,9 +1,8 @@
 package deb_test
 
 import (
+	"fmt"
 	"io"
-	"io/ioutil"
-	"log"
 	"os"
 	"testing"
 
@@ -15,23 +14,23 @@ import (
  */
 
 func isok(t *testing.T, err error) {
+	t.Helper()
 	if err != nil && err != io.EOF {
-		log.Printf("Error! Error is not nil! %s\n", err)
-		t.FailNow()
+		t.Fatalf("Error! Error is not nil! %v", err)
 	}
 }
 
 func notok(t *testing.T, err error) {
+	t.Helper()
 	if err == nil {
-		log.Printf("Error! Error is nil!\n")
-		t.FailNow()
+		t.Fatalf("Error! Error is nil!")
 	}
 }
 
 func assert(t *testing.T, expr bool) {
+	t.Helper()
 	if !expr {
-		log.Printf("Assertion failed!")
-		t.FailNow()
+		t.Fatal("Assertion failed!")
 	}
 }
 
@@ -57,14 +56,14 @@ func TestAr(t *testing.T) {
 	assert(t, firstEntry.OwnerID == 501)
 	assert(t, firstEntry.GroupID == 20)
 
-	firstContent, err := ioutil.ReadAll(firstEntry.Data)
+	firstContent, err := io.ReadAll(firstEntry.Data)
 	isok(t, err)
 	assert(t, firstEntry.Size == int64(len(firstContent)))
 	assert(t, string(firstContent) == "Hello world!\n")
 
 	secondEntry, err := ar.Next()
 	isok(t, err)
-	secondContent, err := ioutil.ReadAll(secondEntry.Data)
+	secondContent, err := io.ReadAll(secondEntry.Data)
 	isok(t, err)
 	assert(t, secondEntry.Size == int64(len(secondContent)))
 	assert(t, string(secondContent) == "I love lamp.\n")
@@ -73,7 +72,59 @@ func TestAr(t *testing.T) {
 	// reading the second one.
 	_, err = firstEntry.Data.Seek(0, 0)
 	isok(t, err)
-	firstRereadContent, err := ioutil.ReadAll(firstEntry.Data)
+	firstRereadContent, err := io.ReadAll(firstEntry.Data)
 	isok(t, err)
 	assert(t, string(firstContent) == string(firstRereadContent))
+}
+
+// `long.a` is taken from gccgoexportdata.
+// It contains empty fields.
+func TestArEmptyFields(t *testing.T) {
+	file, err := os.Open("testdata/long.a")
+	isok(t, err)
+
+	ar, err := deb.LoadAr(file)
+	isok(t, err)
+
+	// First
+	firstEntry, err := ar.Next()
+	isok(t, err)
+
+	assert(t, firstEntry.Name == ``)
+	assert(t, firstEntry.Timestamp == 1478713642)
+	assert(t, firstEntry.OwnerID == 0)
+	assert(t, firstEntry.GroupID == 0)
+	assert(t, firstEntry.Size == 4)
+
+	firstContent, err := io.ReadAll(firstEntry.Data)
+	isok(t, err)
+	assert(t, firstEntry.Size == int64(len(firstContent)))
+	//assert(t, string(firstContent) == "")
+
+	// Second
+	secondEntry, err := ar.Next()
+	isok(t, err)
+
+	assert(t, secondEntry.Name == `/`)
+	assert(t, secondEntry.Timestamp == 0)
+	assert(t, secondEntry.OwnerID == 0)
+	assert(t, secondEntry.GroupID == 0)
+	assert(t, secondEntry.Size == 32)
+
+	secondContent, err := io.ReadAll(secondEntry.Data)
+	isok(t, err)
+	assert(t, secondEntry.Size == int64(len(secondContent)))
+	assert(t, string(secondContent) == "name-longer-than-16-bytes.gox/\n\n")
+
+	// Third
+	thirdEntry, err := ar.Next()
+	isok(t, err)
+	fmt.Printf("%v", thirdEntry)
+	//// Now, test that we can rewind and reread the first file even after
+	//// reading the second one.
+	//_, err = firstEntry.Data.Seek(0, 0)
+	//isok(t, err)
+	//firstRereadContent, err := io.ReadAll(firstEntry.Data)
+	//isok(t, err)
+	//assert(t, string(firstContent) == string(firstRereadContent), "")
 }
